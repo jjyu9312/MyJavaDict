3f171797581765a7537d9b8685fe1570168e1898
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-01-27 18:47:21 +0900

    22/01/27 </br> equals와 hashCode

---
+++ src/Jan/Book.java
@@ -0,0 +1,23 @@
+package Jan;
+
+public class Book {
+
+    private int bookPrice;
+    private String bookName;
+
+    public int getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(int bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+}
--- src/Jan/Equals_HashCode.java
+++ src/Jan/Equals_HashCode.java
@@ -1,4 +1,29 @@
 package Jan;
 
 public class Equals_HashCode {
+    public static void main(String[] args) {
+
+        Book book_1 = new Book();
+        book_1.setBookName("불편한 편의점");
+        book_1.setBookPrice(15000);
+
+        Book book_2 = new Book();
+        book_2.setBookName("불편한 편의점");
+        book_2.setBookPrice(15000);
+
+        System.out.println("book_1.equals(book_2) : " + book_1.equals(book_2)); // false
+        // euqals는 단순히 참조 값 즉, 객체의 주소 값이 같은 지 확인
+
+        System.out.println("book_1.hashCode : " + book_1.hashCode());
+        System.out.println("book_2.hashCode : " + book_2.hashCode());
+        // hashCode는 객체의 메모리 주소를 이용해서 해시코드를 만들어 리턴
+
+        book_1.setBookName("달러구트 꿈 백화점");
+        book_1.setBookPrice(20000);
+
+        System.out.println("=========== 값이 변경된 book_1 =============");
+        System.out.println("book_1.hashCode : " + book_1.hashCode());
+        // book_1의 값이 변경되어도 hashCode 값은 동일 ==> 결국 같은 객체의 book_1 주소를 리턴하기 때문에
+
+    }
 }
