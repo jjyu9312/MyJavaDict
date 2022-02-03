744d62ac71ef41a8037499f8c18b30375e8d9a40
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-02-03 23:41:32 +0900

    22/02/03 \n 참조변수인 super와 this

--- src/fab/Super_This.java
+++ src/fab/Super_This.java
@@ -4,14 +4,14 @@ public class Super_This {
 
     public static void main(String[] args) {
         Coffee c1 = new Coffee("Americano", 500);
-        Coffee c2 = new Coffee("CaffeeLatte", 600);
+        Coffee c2 = new Coffee("IceVanillaLatte", 600);
 
         System.out.println(c1.toString());
         System.out.println(c2.toString());
 
         System.out.println("========================================================");
 
-        Latte c3 = new Latte("CaffeeLatte", 600, 400);
+        Latte c3 = new Latte("IceVanillaLatte", 600, 400);
         System.out.println(c3.toString());
     }
 }
